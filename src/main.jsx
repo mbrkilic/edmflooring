de23656070eb073e75c87b8790b28bb3cf449984
unknown <mdburakkilic@gmail.com>
2025-01-06 20:25:13 +0300

    browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ToastContainer } from 'react-toastify/unstyled'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
     <ToastContainer/>
-  </StrictMode>,
+  </BrowserRouter>,
 )
